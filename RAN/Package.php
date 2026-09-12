67c2fb7cedf14273f44c9fe5e19de8b211e89708
Benjamin Rush <bnjmnrsh@gmail.com>
2026-09-12 12:15:36 +0100

    refactor: expose package artifact limit override

--- RAN/Package.php
+++ RAN/Package.php
@@ -33,6 +33,10 @@ interface Package {
 
 	public function setSource( PackageSource $source, int $revision ): void;
 
+	public function getMaximumArtifactBytes(): ?int;
+
+	public function setMaximumArtifactBytes( ?int $maximumArtifactBytes ): void;
+
 	public function setRepository( ManagedRepository $repository ): void;
 
 	public function getRepository(): ManagedRepository;

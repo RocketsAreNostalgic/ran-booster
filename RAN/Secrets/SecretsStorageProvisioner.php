a71d9850268ed17c8d2ae0ba703798db4fb871b8
Benjamin Rush <bnjmnrsh@gmail.com>
2026-09-18 14:04:35 +0100

    style: align secrets provisioner constants

--- RAN/Secrets/SecretsStorageProvisioner.php
+++ RAN/Secrets/SecretsStorageProvisioner.php
@@ -21,7 +21,7 @@ class SecretsStorageProvisioner {
 	private const DIRECTORY_CONSTANT_NAME        = 'RAN_BOOSTER_ENCRYPTED_SECRETS_DIR';
 	private const UNSUPPORTED_FILE_CONSTANT_NAME = 'RAN_BOOSTER_ENCRYPTED_SECRETS_FILE';
 	private const RECOVERY_MAX_ENTRIES           = 64;
-	public const RESET_CONFIRMATION       = 'RESET STORAGE';
+	public const RESET_CONFIRMATION              = 'RESET STORAGE';
 
 	public function __construct(
 		private readonly PrivateLocationCandidateResolver $resolver = new PrivateLocationCandidateResolver(),
